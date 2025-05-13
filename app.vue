26601572c4c29fead1191838a7fd7e477f9dbd4e
Frano Sincic <frano.sincic@gmail.com>
2025-05-13 16:43:08 +0200

    Added nuxt ui & tailwind

--- app.vue
+++ app.vue
@@ -1,3 +1,10 @@
 <template>
-  <h1>Nuxt App Starter</h1>
+  <UApp>
+    <UCard>
+      <h1 class="flex justify-center items-center">
+        <span class="text-primary mr-2">Nuxt App Starter</span>
+        <UIcon name="mdi:github" />
+      </h1>
+    </UCard>
+  </UApp>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,14 @@
 /* globals defineNuxtConfig */
 
 export default defineNuxtConfig({
-  modules: ['@nuxt/eslint'],
+  modules: ['@nuxt/eslint', '@nuxt/ui'],
   devtools: { enabled: true },
   app: {
     head: {
       title: 'Nuxt App Starter'
     }
   },
+  css: ['~/assets/main.css'],
   compatibilityDate: '2024-11-01',
   eslint: {
     config: {
