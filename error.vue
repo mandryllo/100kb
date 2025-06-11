d182309048dcda9ad25c75657d5b67dc50d3409a
Frano Sincic <frano.sincic@gmail.com>
2025-06-11 19:10:12 +0200

    Added error and catch all pages

---
+++ error.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <NuxtLayout name="default">
+    <h1 class="text-center">Error</h1>
+    <div class="text-center">
+      Something went wrong.
+      Please
+      <ULink
+        @click="clearError()"
+        to="https://github.com/mandryllo/100kb/issues"
+        external
+        target="_blank">
+        open an issue.
+      </ULink>
+    </div>
+  </NuxtLayout>
+</template>
---
+++ pages/[...slug].vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <h1 class="text-center">Not found</h1>
+    <div class="text-center">
+      Page not found. Please go to <ULink to="/">Home.</ULink>
+    </div>
+  </div>
+</template>
